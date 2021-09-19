67cee6c248c0d401013b246ff4253246bc18c93c
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2021-09-19 12:59:53 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,47 @@
+canvas=document.getElementById("myCanvas");
+ctx=canvas.getContext("2d");
+rover_width=100;
+rover_height=90;
+rover_x=10;
+rover_y=10;
+background_img="mars.jpg";
+rover_img="rover.png";
+function add(){
+    background_imgTag=new Image();
+    background_imgTag.onload=uploadBackground();
+    background_imgTag.src=background_img;
+
+    rover_imgTag=new Image();
+    rover_imgTag.onload=uploadRover();
+    rover_imgTag.src=rover_img;
+}
+function uploadBackground(){
+    ctx.drawImage(background_imgTag, 0, 0,canvas.width , canvas.height);
+}
+function uploadRover(){
+    ctx.drawImage(rover_imgTag, rover_x, rover_y, rover_width, rover_height);
+}
+window.addEventListener("keydown",my_keydown);
+function my_keydown(e){
+    keypress=e.keyCode;
+    if(keypress == '38'){
+        up();
+        console.log("up");
+
+    }
+    if(keypress == '40'){
+        down();
+        console.log("down");
+        
+    }
+    if(keypress == '37'){
+        left();
+        console.log("left");
+        
+    }
+    if(keypress == '39'){
+        right();
+        console.log("right");
+        
+    }
+}
