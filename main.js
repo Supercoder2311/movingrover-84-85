61b3a0b7a2d6dc15717168574013082c6f023876
Supercoder2311 <84426763+Supercoder2311@users.noreply.github.com>
2021-09-26 12:43:45 -0400

    Add files via upload

--- main.js
+++ main.js
@@ -4,7 +4,11 @@ rover_width=100;
 rover_height=90;
 rover_x=10;
 rover_y=10;
-background_img="mars.jpg";
+img_nasa=["Img_1.jpg", "img_2.jpg", "img_3.jpg", "img_4.jpg"];
+
+random_number=Math.floor(Math.random()* 4);
+
+background_img=img_nasa[random_number];
 rover_img="rover.png";
 function add(){
     background_imgTag=new Image();
@@ -43,5 +47,33 @@ function my_keydown(e){
         right();
         console.log("right");
         
+    }
+}
+function up(){
+    if(rover_y >=0){
+        rover_y= rover_y -10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function down(){
+    if(rover_y <=500){
+        rover_y= rover_y +10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function left(){
+    if(rover_x >=0){
+        rover_x= rover_x -10;
+        uploadBackground();
+        uploadRover();
+    }
+}
+function right(){
+    if(rover_x <=700){
+        rover_x= rover_x +10;
+        uploadBackground();
+        uploadRover();
     }
 }
